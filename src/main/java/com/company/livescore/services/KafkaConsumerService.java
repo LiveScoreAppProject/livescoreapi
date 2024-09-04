bc24e1d88addf473ba3bb8314c94d98b6a23ac90
mamamia140 <m-h-k140@hotmail.com>
2024-09-04 10:29:51 +0300

    changed consume method to take key value as parameter as well as value

--- src/main/java/com/company/livescore/services/KafkaConsumerService.java
+++ src/main/java/com/company/livescore/services/KafkaConsumerService.java
@@ -1,5 +1,6 @@
 package com.company.livescore.services;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import com.company.livescore.websocket.WebSocketHandler;
@@ -14,20 +15,14 @@ public class KafkaConsumerService {
     }
 
     @KafkaListener(topics = "live_events", groupId = "event-consumer-group")
-    public void listen(String message) {
+    public void listen(ConsumerRecord<Integer, String> record) {
+        Integer key = record.key();
+        String value = record.value();
 
-        String[] parts = message.split(":", 2);
-        if (parts.length == 2) {
-            try {
-                int key = Integer.parseInt(parts[0]);
-                String event = parts[1];
-                webSocketHandler.broadcastMessage(key, event);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid key in Kafka message: " + parts[0]);
-            }
+        if (key != null && value != null) {
+            webSocketHandler.broadcastMessage(key, value);
         } else {
-
-            System.out.println("Invalid message format: " + message);
+            System.out.println("Received record with null key or value");
         }
     }
 }
